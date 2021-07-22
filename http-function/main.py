1e6018db5bd7f63c3006463fa96dd2cde6b6f5c1
nelsonmavi <nelsonmavi@gmail.com>
2021-07-22 20:07:44 +0200

    sample http-function

---
+++ http-function/main.py
@@ -0,0 +1,20 @@
+from flask import escape
+
+
+def greetings_content(request):
+    content_type = request.headers['content-type']
+    if content_type == 'application/json':
+        request_json = request.get_json(silent=True)
+        if request_json and 'name' in request_json:
+            name = request_json['name']
+        else:
+            raise ValueError("Json is invalid, or missing a 'name' property")
+    elif content_type == 'application/octet-stream':
+        name = request.data
+    elif content_type == 'text/plain':
+        name = request.data
+    elif content_type == 'application/x-www-form-urlencoded':
+        name = request.form.get('name')
+    else:
+        raise ValueError("Unknown content type: {}".format(content_type))
+    return 'Greetings, {}'.format(escape(name))
